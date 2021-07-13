ceb3bfcdfb1c7d44aa2d75c091712ba346f64695
Jorengarenar <jorengarenar@outlook.com>
2021-07-13 19:17:15 +0200

    Reddit Old - Better Markdown

---
+++ Reddit/Better_Markdown.user.js
@@ -0,0 +1,116 @@
+// ==UserScript==
+// @name         Reddit Old - Better Markdown
+// @decription   Replace Markdown renderer on Old Reddit with Marked
+// @version      0.9.0
+// @author       Jorengarenar
+// @run-at       document-start
+// @require      https://cdn.jsdelivr.net/npm/marked/marked.min.js
+// @match        https://*.reddit.com/r/*/*
+// @match        https://*.reddit.com/user/*
+// ==/UserScript==
+
+const spoiler = {
+  name: "spoiler",
+  level: "inline",
+  start(src) { return src.match(/>!/)?.index; },
+  tokenizer(src, tokens) {
+    const rule = /^>!(.*?)!</;
+    const match = rule.exec(src);
+    if (match) {
+      return {
+        type: "spoiler",
+        raw: match[0],
+        text: match[1],
+        tokens: this.inlineTokens(match[1])
+      };
+    }
+  },
+  renderer(token) {
+    return `<span class="md-spoiler-text">${this.parseInline(token.tokens)}</span>`; // parseInline to turn child tokens into HTML
+  }
+}
+
+const superscript = {
+  name: "superscript",
+  level: "inline",
+  start(src) { return src.match(/\^/)?.index; },
+  tokenizer(src, tokens) {
+    const rule = /^\^(\((.*?)\)|(\S+))/;
+    const match = rule.exec(src);
+    if (match) {
+      const txt = match[2] ? match[2] : match[3];
+      return {
+        type: "superscript",
+        raw: match[0],
+        text: txt,
+        tokens: this.inlineTokens(txt)
+      };
+    }
+  },
+  renderer(token) {
+    return `<sup>${this.parseInline(token.tokens)}</sup>`;
+  }
+}
+
+const subreddit = {
+  name: "subreddit",
+  level: "inline",
+  start(src) { return src.match(/\/?[ru]\//)?.index; },
+  tokenizer(src, tokens) {
+    const rule = /^\/?([ru]\/\w+)/;
+    const match = rule.exec(src);
+    if (match) {
+      return {
+        type: "subreddit",
+        raw: match[0],
+        text: match[1],
+      };
+    }
+  },
+  renderer(token) {
+    return `<a href="/${token.text}">${token.text}</a>`;
+  }
+}
+
+marked.use({ extensions: [ spoiler, superscript, subreddit ] });
+
+
+function recodeHTML(html) {
+  let txt = document.createElement("textarea");
+  txt.innerHTML = html;
+  return txt.value.replace(/<(.+)>(.*)<\/\1>/g, '&lt;$1&gt;$2&lt;\/$1&gt;');
+};
+
+function genMd(d) {
+  d.data.children.forEach(c => {
+    if (c.kind === "t1" || c.kind === "t3") {
+      let md = document.querySelector(`#thing_${c.kind}_${c.data.id} > .entry > ` +
+                                      `${c.kind === "t3" ? "div >" : ""} form > ` +
+                                      `.usertext-body > .md:not(.marked)`);
+      if (md) {
+        const text = c.kind === "t3" ? c.data.selftext : c.data.body;
+        md.innerHTML = marked(recodeHTML(text)); // unsure whether sanitization will be necessary?
+        md.classList.add("marked");
+      }
+    }
+    if (c.data.replies) { genMd(c.data.replies); }
+  });
+}
+
+async function Markdown() {
+  const response = await fetch(window.location.href + ".json");
+  const json = await response.json();
+  if (json.length) {
+    json.forEach(genMd);
+  } else {
+    genMd(json);
+  }
+}
+
+Markdown();
+
+window.onload = function() {
+  new MutationObserver(function() {
+    if (document.querySelector(".thing .usertext-body > .md:not(.marked)")) { Markdown(); }
+  }).observe( document.querySelector('.content[role="main"]'), { childList: true, subtree: true } );
+};
