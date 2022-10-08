246ba21472b42fd1c31dddec6cf011e433a5e0f4
Jorengarenar <jorengarenar@outlook.com>
2022-10-09 00:43:15 +0200

    RegEx correction

--- Reddit/Better_Markdown.user.js
+++ Reddit/Better_Markdown.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Markdown for Old Reddit
 // @description  Replace Markdown renderer on Old Reddit with Marked
-// @version      1.2.4
+// @version      1.2.5
 // @author       Jorengarenar
 // @run-at       document-start
 // @require      https://cdn.jsdelivr.net/npm/marked@4.1.1/marked.min.js
@@ -184,7 +184,7 @@ function genMd(d) {
         markdown = markdown.replace(/^ {0,3}>!/gm, "@>!"); // prevents confusion with comment
         markdown = markdown.replace(/([^\n])\n\s*```(\S+?)?$/gm, "$1\n\n```"); // fix code fence without empty line above
         markdown = markdown.replace(/(```.*?\n.*?[^\n])```/gms, "$1\n```"); // fix ending code fence not in new line
-        markdown = markdown.replace(/^(#+)(\S)/gm, "$1 $2"); // add space after header #s in case it's missing
+        markdown = markdown.replace(/^(#+)([^# ])/gm, "$1 $2"); // add space after header #s in case it's missing
 
         md.innerHTML = marked.parse(markdown);
         md.classList.add("marked");
